9f263ec8714a6a10b37de324fcf20754c7e94c14
kwokhue80 <226826856+kwokhue80@users.noreply.github.com>
2026-04-01 23:50:09 +0800

    Update EmployeeService.java
    added a findAll() method

--- src/main/java/sg/edu/nus/laps/employee/service/EmployeeService.java
+++ src/main/java/sg/edu/nus/laps/employee/service/EmployeeService.java
@@ -1,8 +1,24 @@
 package sg.edu.nus.laps.employee.service;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
+
+import sg.edu.nus.laps.employee.model.Employee;
+import sg.edu.nus.laps.employee.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
+	
+	private final EmployeeRepository eRepo;
 
+	public EmployeeService(EmployeeRepository eRepo) {
+		super();
+		this.eRepo = eRepo;
+	};
+	
+	public List<Employee> findAll() {
+		return eRepo.findAll();
+	}
 }
