4a336b05adb5113785f87855e9218f76ba285855
kwokhue80 <226826856+kwokhue80@users.noreply.github.com>
2026-04-04 02:11:56 +0800

    Update EmployeeService.java
    -created findByEmail, findById, save() and delete() methods to call EmployeeRepository

--- src/main/java/sg/edu/nus/laps/employee/service/EmployeeService.java
+++ src/main/java/sg/edu/nus/laps/employee/service/EmployeeService.java
@@ -3,7 +3,6 @@ package sg.edu.nus.laps.employee.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Service;
 
 import sg.edu.nus.laps.employee.model.Employee;
@@ -17,17 +16,33 @@ public class EmployeeService {
 	public EmployeeService(EmployeeRepository eRepo) {
 		super();
 		this.eRepo = eRepo;
-	};
+	}
 	
 	public List<Employee> findAll() {
 		return eRepo.findAll();
 	}
 	
-	public Optional<Employee> findByEmail() {
-		Optional<Employee> emOpt = eRepo.findByEmail();
+	public Optional<Employee> findByEmail(String email) {
+		Optional<Employee> emOpt = eRepo.findByEmail(email);
 		if (emOpt.isPresent()) {
 			return emOpt;
 		}
-		return emOpt.empty();
+		return Optional.empty();
+	}
+	
+	public Optional<Employee> findById(Long id) {
+		Optional<Employee> empOpt = eRepo.findById(id);
+		if (empOpt.isPresent()) {
+			return empOpt;
+		}
+		return Optional.empty();
+	}
+	
+	public void save(Employee employee) {
+		eRepo.save(employee);
+	}
+	
+	public void delete(Employee employee) {
+		eRepo.delete(employee);
 	}
 }
