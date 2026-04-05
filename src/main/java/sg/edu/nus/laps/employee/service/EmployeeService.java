8caadc423b2238956f7bd22fb070268ce59a0e05
kwokhue80 <226826856+kwokhue80@users.noreply.github.com>
2026-04-05 21:29:04 +0800

    Added @Transactional and @Modifying
    -Added @Transactional for all methods
    -Added @Modifying on save and delete methods

--- src/main/java/sg/edu/nus/laps/employee/service/EmployeeService.java
+++ src/main/java/sg/edu/nus/laps/employee/service/EmployeeService.java
@@ -3,7 +3,11 @@ package sg.edu.nus.laps.employee.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import sg.edu.nus.laps.employee.model.Employee;
 import sg.edu.nus.laps.employee.repository.EmployeeRepository;
@@ -18,10 +22,12 @@ public class EmployeeService {
 		this.eRepo = eRepo;
 	}
 	
+	@Transactional(readOnly=true)
 	public List<Employee> findAll() {
 		return eRepo.findAll();
 	}
 	
+	@Transactional(readOnly=true)
 	public Optional<Employee> findByEmail(String email) {
 		Optional<Employee> emOpt = eRepo.findByEmail(email);
 		if (emOpt.isPresent()) {
@@ -30,6 +36,7 @@ public class EmployeeService {
 		return Optional.empty();
 	}
 	
+	@Transactional(readOnly=true)
 	public Optional<Employee> findById(Long id) {
 		Optional<Employee> empOpt = eRepo.findById(id);
 		if (empOpt.isPresent()) {
@@ -38,10 +45,14 @@ public class EmployeeService {
 		return Optional.empty();
 	}
 	
+	@Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.READ_COMMITTED)
+	@Modifying(clearAutomatically = true)
 	public void save(Employee employee) {
 		eRepo.save(employee);
 	}
 	
+	@Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.READ_COMMITTED)
+	@Modifying(clearAutomatically = true)
 	public void delete(Employee employee) {
 		eRepo.delete(employee);
 	}
