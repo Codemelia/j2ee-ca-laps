debb29cc9bdedfeefd401d4bfec92666d25df713
K4i-Z3r <kamaruzaman.khairulanwar@outlook.com>
2026-04-11 22:29:54 +0800

    Added Import Link to SetCreatedUpdated

--- src/main/java/sg/edu/nus/laps/leave/model/LeaveType.java
+++ src/main/java/sg/edu/nus/laps/leave/model/LeaveType.java
@@ -14,6 +14,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import sg.edu.nus.laps.common.util.SetCreatedUpdated;
 
 @Entity
 @Table(name = "leave_type")
