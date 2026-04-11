96f43025f8fc3658b93f3e20a3d7eb3202f0dffa
OngZiLin <ongzilin.a@u.nus.edu>
2026-04-11 16:04:20 +0800

    Update Holiday.java

--- src/main/java/sg/edu/nus/laps/leave/model/Holiday.java
+++ src/main/java/sg/edu/nus/laps/leave/model/Holiday.java
@@ -1,10 +1,13 @@
 package sg.edu.nus.laps.leave.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 
 @Entity
 @Table(name = "holidays")
@@ -14,4 +17,40 @@ public class Holiday {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(name = "name", nullable = false, length = 50)
+	private String name;
+
+	@Column(name = "date", nullable = false)
+	private LocalDateTime date;
+
+	// Default Constructor
+	public Holiday() {
+	}
+
+	// Parameterized Constructor
+	public Holiday(String name, LocalDateTime date) {
+		this.name = name;
+		this.date = date;
+	}
+
+	// Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
 }
