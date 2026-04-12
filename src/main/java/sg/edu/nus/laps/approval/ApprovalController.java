fa14696d703c85048096e32be40597c9f82370d3
Codemelia <ameliaw2903@gmail.com>
2026-04-13 02:30:51 +0800

    ADD template html for My Leaves + Team Leaves

--- src/main/java/sg/edu/nus/laps/approval/ApprovalController.java
+++ src/main/java/sg/edu/nus/laps/approval/ApprovalController.java
@@ -10,13 +10,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
     ------------------------------------------------
     GET /manager/subordinates/{employeeId}/leaves
         - Display leave history for a subordinate
-    GET /manager/leaves
+    GET /manager/team-leaves
         - Display all team leave requests pending/reviewed
-    GET /manager/leaves/{leaveId}
+    GET /manager/team-leaves/{leaveId}
         - Display information of specific leave request
-    POST /manager/leaves/{leaveId}/approve
+    POST /manager/team-leaves/{leaveId}/approve
         - Process leave request approval
-    POST /manager/leaves/{leaveId}/reject
+    POST /manager/team-leaves/{leaveId}/reject
         - Process leave request rejection
 */
 @RequestMapping("/manager")
