ff45fdd4b63d95c7159f808fa2e8eb9f0b65ea93
Codemelia <ameliaw2903@gmail.com>
2026-04-13 01:14:15 +0800

    CLEANUP code

--- src/main/java/sg/edu/nus/laps/auth/AuthController.java
+++ src/main/java/sg/edu/nus/laps/auth/AuthController.java
@@ -3,11 +3,16 @@ package sg.edu.nus.laps.auth;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
+import sg.edu.nus.laps.auth.security.AuthUserDetails;
 
 /*
     AuthController handles all user auth operations
@@ -44,9 +49,9 @@ public class AuthController {
 
         // Add params based on param value
         if (unauth != null) { params.add("unauthorised"); }
-        if (error != null) { params.add("unauthorised"); }
-        if (logout != null) { params.add("unauthorised"); }
-        if (expired != null) { params.add("unauthorised"); }
+        if (error != null) { params.add("error"); }
+        if (logout != null) { params.add("logout"); }
+        if (expired != null) { params.add("expired"); }
 
         // Build and return final redir string
         if (!params.isEmpty()) { redirect.append("?").append(String.join("&", params)); }
@@ -66,44 +71,5 @@ public class AuthController {
         model.addAttribute("entryPoint", "admin");
         return "auth/login";
     }
-
-    // REPLACE WITH SPRING SECURITY
-    // // POST /auth/login
-    // @PostMapping("/login")
-    // public String postLogin(@Valid @ModelAttribute _LoginRequest loginUser, 
-    //     Model model, HttpSession session, BindingResult bindRes) {
-
-    //     // If session contains user, redirect to index page
-    //     if (session.getAttribute("userEmail") != null)
-    //         return "redirect/";
-
-    //     // If there are validation errors, return to login page with errors
-    //     if (bindRes.hasErrors()) return "login";
-
-    //     // Authenticate user email and password
-    //     // boolean auth = authSvc.authenticate(loginUser.getEmail(), loginUser.getPassword());
-        
-    //     // // If invalid credentials, return error message to login page
-    //     // if (!auth) {
-    //     //     model.addAttribute("errorMsg", "Your login credentials were invalid. Please try again.");
-    //     //     model.addAttribute("email", loginUser.getEmail()); // Re-populate form without password for security
-    //     //     return "login";
-    //     // } 
-        
-    //     // // If valid credentials, save user email to session (no password for security)
-    //     // // Redirect to index page
-    //     // else {
-    //     //     session.setAttribute("userEmail", loginUser.getEmail());
-    //     //     return "redirect:/";
-    //     // }
-
-    //     return "";
-    // }
-
-    // // POST /auth/logout
-    // @PostMapping("/logout")
-    // public void postLogout(HttpSession session) {
-    //     session.invalidate(); // Invalidate session
-    // }
 
 }
