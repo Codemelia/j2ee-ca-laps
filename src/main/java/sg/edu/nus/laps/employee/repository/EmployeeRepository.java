b828758917e8ab4777c1e1bf7e8ce425a5ca3065
kwokhue80 <226826856+kwokhue80@users.noreply.github.com>
2026-04-14 23:52:37 +0800

    added Pageable

--- src/main/java/sg/edu/nus/laps/employee/repository/EmployeeRepository.java
+++ src/main/java/sg/edu/nus/laps/employee/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package sg.edu.nus.laps.employee.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,5 +20,6 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 	@Query("select count(e.Id) from Employee e")
 	Integer countId();
 	
-
+	Page<Employee> findAll(Pageable pageable);
+	
 }
