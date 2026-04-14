fab002a75eabdfcf9c0fb0693daeee712e0900f8
kwokhue80 <226826856+kwokhue80@users.noreply.github.com>
2026-04-14 19:20:04 +0800

    added constructor and method to display displayName for enum values

--- src/main/java/sg/edu/nus/laps/employee/model/EmployeeRank.java
+++ src/main/java/sg/edu/nus/laps/employee/model/EmployeeRank.java
@@ -1,6 +1,16 @@
 package sg.edu.nus.laps.employee.model;
 
 public enum EmployeeRank {
-	NON_EXECUTIVE,
-	PROFESSIONAL
+	NON_EXECUTIVE("Non-Executive"),
+	PROFESSIONAL("Professional");
+	
+	private final String displayName;
+	
+	EmployeeRank(String displayName) {
+		this.displayName = displayName;
+	}
+	
+	public String getDisplayName() {
+		return displayName;
+	}
 }
