8372ceba5db1109168b148230dcb3881c0892804
Codemelia <ameliaw2903@gmail.com>
2026-04-14 20:28:35 +0800

    UPDATE SpringSecurity request authorisation

--- src/main/java/sg/edu/nus/laps/auth/AuthController.java
+++ src/main/java/sg/edu/nus/laps/auth/AuthController.java
@@ -29,40 +29,40 @@ public class AuthController {
     // @Autowired
     // private UserService userSvc;
 
-    // USING SPRING SECURITY
-    // Fallback - GET /auth/login
-    @GetMapping("/login")
-    public String getLogin(
-        @RequestParam(required = false) String unauth,
-        @RequestParam(required = false) String error,
-        @RequestParam(required = false) String logout,
-        @RequestParam(required = false) String expired,
-        HttpServletRequest request) {
+    // // USING SPRING SECURITY
+    // // Fallback - GET /auth/login
+    // @GetMapping("/login")
+    // public String getLogin(
+    //     @RequestParam(required = false) String unauth,
+    //     @RequestParam(required = false) String error,
+    //     @RequestParam(required = false) String logout,
+    //     @RequestParam(required = false) String expired,
+    //     HttpServletRequest request) {
 
-        // Build base redirect string - default to employee login for employee/manager
-        StringBuilder redirect = new StringBuilder("redirect:/auth/employee/login");
+    //     // Build base redirect string - default to employee login for employee/manager
+    //     StringBuilder redirect = new StringBuilder("redirect:/auth/employee/login");
 
-        // Retrieve referer from request
-        String ref = request.getHeader("Referer");
+    //     // Retrieve referer from request
+    //     String ref = request.getHeader("Referer");
         
-        // If user was trying to access admin pages, redirect to admin instead
-        if (ref != null && ref.contains("/admin")) {
-            redirect = new StringBuilder("redirect:/auth/admin/login");
-        }
+    //     // If user was trying to access admin pages, redirect to admin instead
+    //     if (ref != null && ref.contains("/admin")) {
+    //         redirect = new StringBuilder("redirect:/auth/admin/login");
+    //     }
 
-        // set to hold param
-        Set<String> params = new HashSet<>();
+    //     // set to hold param
+    //     Set<String> params = new HashSet<>();
 
-        // Add params based on param value
-        if (unauth != null) { params.add("unauthorised"); }
-        if (error != null) { params.add("error"); }
-        if (logout != null) { params.add("logout"); }
-        if (expired != null) { params.add("expired"); }
+    //     // Add params based on param value
+    //     if (unauth != null) { params.add("unauthorised"); }
+    //     if (error != null) { params.add("error"); }
+    //     if (logout != null) { params.add("logout"); }
+    //     if (expired != null) { params.add("expired"); }
 
-        // Build and return final redir string
-        if (!params.isEmpty()) { redirect.append("?").append(String.join("&", params)); }
-        return redirect.toString();
-    }
+    //     // Build and return final redir string
+    //     if (!params.isEmpty()) { redirect.append("?").append(String.join("&", params)); }
+    //     return redirect.toString();
+    // }
 
     // ASSIGNMENT: 2 ENTRY POINTS
 
