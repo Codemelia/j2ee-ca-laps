5def93e1574070ce70763311f139f402434f0590
CL-Y26 <chailee.yeo@u.nus.edu>
2026-04-14 19:05:02 +0800

    remove additional codes on LeaveService model

--- src/main/java/sg/edu/nus/laps/leave/service/LeaveService.java
+++ src/main/java/sg/edu/nus/laps/leave/service/LeaveService.java
@@ -9,13 +9,13 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-<<<<<<< Updated upstream
+
 
 import sg.edu.nus.laps.employee.model.Employee;
 import sg.edu.nus.laps.employee.model.EmployeeRank;
-=======
+
 import org.springframework.data.domain.Pageable;
->>>>>>> Stashed changes
+
 import sg.edu.nus.laps.employee.repository.EmployeeRepository;
 import sg.edu.nus.laps.leave.model.LeaveApplication;
 import sg.edu.nus.laps.leave.model.LeaveRecord;
