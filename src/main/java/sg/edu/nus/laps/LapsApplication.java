485c06424284156ecebc24ebd1a728001c75f29a
K4i-Z3r <kamaruzaman.khairulanwar@outlook.com>
2026-04-15 16:15:53 +0800

    Add @EnableScheduling since @Scheduled is Implemented in HolidayService

--- src/main/java/sg/edu/nus/laps/LapsApplication.java
+++ src/main/java/sg/edu/nus/laps/LapsApplication.java
@@ -2,8 +2,10 @@ package sg.edu.nus.laps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LapsApplication {
 
 	public static void main(String[] args) {
