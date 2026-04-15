bba436386806b40bf0bfc4e3dd0b886b20240562
K4i-Z3r <kamaruzaman.khairulanwar@outlook.com>
2026-04-15 16:33:35 +0800

    Added @PostConstruct and @Scheduled Annotation

--- src/main/java/sg/edu/nus/laps/leave/service/HolidayService.java
+++ src/main/java/sg/edu/nus/laps/leave/service/HolidayService.java
@@ -1,10 +1,12 @@
 package sg.edu.nus.laps.leave.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
+import jakarta.annotation.PostConstruct;
 import sg.edu.nus.laps.leave.dto.DataGovResponse;
 import sg.edu.nus.laps.leave.model.Holiday;
 import sg.edu.nus.laps.leave.repository.HolidayRepository;
@@ -16,11 +18,19 @@ public class HolidayService {
 	private HolidayRepository holidayRepo;
 	
 	private final String DATASET_ID = "d_149b61ad0a22f61c09dc80f2df5bbec8";
-	private final String BASE_URL = "https://data.gov.sg/api/action/datastore_search?resource_id=" + DATASET_ID;
+	private final String BASE_URL = "https://data.gov.sg/api/action/datastore_search?resource_id=" + DATASET_ID + "&limit=100";
 	
 	private final RestTemplate restTemplate = new RestTemplate();
 	
+	@PostConstruct
+	public void init() {
+		if (holidayRepo.count() == 0) {
+			fetchAndSyncHolidays();
+		}
+	}
+	
 	@Transactional
+	@Scheduled(cron = "0 0 0 1 1 ?")
 	public void fetchAndSyncHolidays() {
 		try {
 			// 1. FETCH JSON from data.gov.sg
