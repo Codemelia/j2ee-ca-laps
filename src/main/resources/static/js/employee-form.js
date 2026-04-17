ab9af263ab49e22a6269f0a4d7e83f7f7e87aae4
kwokhue80 <226826856+kwokhue80@users.noreply.github.com>
2026-04-17 18:26:45 +0800

    amended logic for AL entitlement field

--- src/main/resources/static/js/employee-form.js
+++ src/main/resources/static/js/employee-form.js
@@ -9,12 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
 		        case 'NON_EXECUTIVE':
 					leaveInput.setAttribute('min', '14.0');
 					leaveInput.setAttribute('max', '17.0');
-		            leaveInput.value = 14.0;
+					if(leaveInput.value == '' || leaveInput.value > 17.0) {
+						leaveInput.value = 14;
+					}
 		            break;
 		        case 'PROFESSIONAL':
 					leaveInput.setAttribute('min', '18');
 					leaveInput.setAttribute('max', '21');
-		            leaveInput.value = 18.0;
+					if(leaveInput.value == '' || leaveInput.value < 18.0) {
+						leaveInput.value = 18;
+					};
 		            break;
 		        default:
 		            leaveInput.value = ''; 
