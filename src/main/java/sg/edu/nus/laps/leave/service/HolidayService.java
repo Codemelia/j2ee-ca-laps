762338c0a6d40b737f3767273f20bf76122760be
Codemelia <ameliaw2903@gmail.com>
2026-04-19 18:33:09 +0800

    UNCOMMENT Retrieve Holiday API on app start

--- src/main/java/sg/edu/nus/laps/leave/service/HolidayService.java
+++ src/main/java/sg/edu/nus/laps/leave/service/HolidayService.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.laps.leave.service;
 
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 // import org.springframework.boot.context.event.ApplicationReadyEvent;
 // import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -44,7 +46,7 @@ public class HolidayService {
 	 * 		of the new year. Does not include unscheduled public holidays such as Polling Day. 
 	 */
 	@Transactional
-	// @EventListener(ApplicationReadyEvent.class) // Run once every time app starts (seeding)
+	@EventListener(ApplicationReadyEvent.class) // Run once every time app starts (seeding)
 	@Scheduled(cron = "0 0 0 1 1 ?")
 	public void fetchAndSyncHolidays() {
 		try {
