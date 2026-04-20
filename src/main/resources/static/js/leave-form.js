04ef9b86bdb79b8d19697c3ef4fa2a8ee2fba409
K4i-Z3r <kamaruzaman.khairulanwar@outlook.com>
2026-04-20 14:59:49 +0800

    Added AM/PM Selector for Compensation Leave Application

--- src/main/resources/static/js/leave-form.js
+++ src/main/resources/static/js/leave-form.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const leaveTypeSelect = document.getElementById('leaveType');
 	const proofStar = document.getElementById('proof-star');
+	const compToggles = document.querySelectorAll(".compensation-toggle");
 	
 	function toggleproofStar() {
 		const selectedText = leaveTypeSelect.options[leaveTypeSelect.selectedIndex].text;
@@ -19,5 +20,20 @@ if(leaveTypeSelect) {
 	
 	leaveTypeSelect.addEventListener('change',toggleproofStar);
 }
+
+	function toggleCompensationFields() {
+		// "3" is the ID for Compensation Leave
+		if (leaveTypeSelect.value === "3") {
+			compToggles.forEach(el => el.style.display = "inline-block");
+		} else {
+			compToggles.forEach(el => el.style.display = "none");
+		}
+	}
+	
+	// Run on change
+	leaveTypeSelect.addEventListener("change", toggleCompensationFields);
+
+	// Run on page load (important for Edit mode/Drafts)
+	toggleCompensationFields();
 
 });
